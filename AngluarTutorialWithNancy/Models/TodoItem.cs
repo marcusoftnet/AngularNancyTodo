92c7751728bb5364c9c3c7467a4f759ee494b7bd
marcusoftnet <marcusoft.net@gmail.com>
2013-04-12 21:10:31 +0200

    Backend first method up

---
+++ AngluarTutorialWithNancy/Models/TodoItem.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AngluarTutorialWithNancy.Models
+{
+    public class TodoItem
+    {
+        public int Id { get; set; }
+        public string Todo { get; set; }
+        public byte Priority { get; set; }
+        public DateTime? DueDate { get; set; }
+    }
+}
---
+++ AngluarTutorialWithNancy/Modules/TodoModule.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using AngluarTutorialWithNancy.Models;
+using Nancy;
+using Simple.Data;
+
+namespace AngluarTutorialWithNancy.Modules
+{
+    public class TodoModule : NancyModule
+    {
+        private dynamic _db = Database.Open();
+
+        public TodoModule()
+        {
+            Get["/"] = _ => { return View["Index", GetAllTodos()]; };
+        }
+
+        private List<TodoItem> GetAllTodos()
+        {
+            return _db.TodoItems.All();
+        }
+    }
+
+}
